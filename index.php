d34d1c56e47aca3050d8d0721093b9fc7dd17278
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-12-29 23:04:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
+<!DOCTYPE html>
 <html>
-  <title>dynymic test</title>
   <head>
+  <title>dynymic test</title>
   <link rel="stylesheet" href="styles.css">
   </head>
+
 <body>
 <header>
     <div class="container">
@@ -17,5 +19,29 @@
       </nav>
     </div>
   </header>
+<footer>
+    <div class="container">
+      <h1 class="logo"></h1>
+      <nav>
+        <ul>
+          <li><a href="#">name|street|phone|fax|mail|</a></li>
+        </ul>
+      </nav>
+    </div>
+  
+</body>
+</html>
+
+
+
+<html lang="en">
+<head>
+  <title>Title of the document</title>
+</head>
+<body>
+
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
 </body>
 </html>
