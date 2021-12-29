ed36df739686d92f072a592567e089d422db95eb
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-12-29 22:54:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <html>
   <title>dynymic test</title>
 <body>
-<?php
-phpinfo();
-?>
+<header>
+    <div class="container">
+      <h1 class="logo"></h1>
+
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </body>
 </html>
